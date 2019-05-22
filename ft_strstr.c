1282db4e8a14bffb09f4a6ead7297087f8bfa7ec
Jordan Rheeder <jrheeder@c3r8s1.wethinkcode.co.za>
2019-05-22 17:00:19 +0200

    re-created strstr function as ft_strstr for LIBFT

---
+++ ft_strstr.c
@@ -0,0 +1,38 @@
+#include "libft.h"
+#include <stdio.h> // remove this
+
+char	*ft_strstr(const char *haystack, const char	*needle)
+{
+	int		i;
+	int		j;
+	char	*result;
+
+	i = 0;
+	j = 0;
+
+	result = (char *)haystack;
+	while (result[i] != '\0')
+	{
+		while (needle[j] == result[i + j])
+		{
+			if (needle[j + 1] == '\0')
+			{
+				return (result + i);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	const char	haystack[1024] = "Step through doors";
+	const char	needle[] = "door";
+	char		*ret;
+
+	ret = ft_strstr(haystack, needle);
+	printf("The substring is \n%s\n", ret);
+	return (0);
+}
