77042f20fb62a34c512be026df87665f211173aa
Jordan Rheeder <jrheeder@c3r8s1.wethinkcode.co.za>
2019-05-22 16:41:25 +0200

    re-created the function strrchr.

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,15 +6,16 @@ char	*ft_strrchr(const char *str, int c)
 {
 	char	*s;
 	int		index;
-	
+		
 	s = (char *)str;
 	index = ft_strlen(s);
 	while (index > 0)
 	{
-		if (s[index +1] == (char)c)
+		if (s[index + 1] == (char)c)
 		{
-			printf("Works");
+			return (&s[index + 1]);
 		}
+		index--;
 	}
 	return (0);
 }
@@ -22,7 +23,7 @@ char	*ft_strrchr(const char *str, int c)
 int	main()
 {
 	int	len;
-	const char str[] = "www.john.com";
+	const char str[] = "www.john..com";
 	const char ch = '.';
 	char *ret;
 
