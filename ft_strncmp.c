a3f19039d972f210d0bde6b3d4240d2c6984f102
Jordan Rheeder <jrheeder@c3r1s2.wethinkcode.co.za>
2019-05-23 00:37:27 +0200

    Updated a few things... still need to work on memccpy.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,49 +1,50 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrheeder <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/22 23:52:11 by jrheeder          #+#    #+#             */
-/*   Updated: 2019/05/22 23:53:41 by jrheeder         ###   ########.fr       */
+/*   Created: 2019/05/23 00:34:14 by jrheeder          #+#    #+#             */
+/*   Updated: 2019/05/23 00:34:15 by jrheeder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-int		ft_strcmp(const char *s1, const char *s2)
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int				i;
-	int				total;
-	
-	total	= 0;
-	i		= 0;
-	while (s1[i] != '\0')
-	{
-		if (s1[i] != s2[i])
+		//size_t		i;
+		int			i;
+		int			total;
+
+		total	= 0;
+		i		= 0;
+		while (s1[i] != '\0' && i < n)
 		{
-			total =  (s1[i] - s2[i]);
-			break ;
+			if (s1[i] != s2[i])
+			{
+				total = (s1[i] - s2[i]);
+				break;
+			}
+			i++;
 		}
-		i++;
-	}
-	return (total);
+		return (total);
 }
-
-
-
 
 
 int	main(void)
 {
-	const char	str1[] = "A";
-	const char	str2[] = "A";
+	const char	str1[] = "Aa";
+	const char	str2[] = "BAA";
 	int	ret;
-	
-	ret = ft_strcmp(str1, str2);
-	
+	size_t	n;
+
+	n = 1;
+
+	ret = ft_strncmp(str1, str2, n);
+
 
 	//Check what the returned result is by making use of if & else statements
 	if (ret < 0)
